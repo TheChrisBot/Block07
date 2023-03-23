7bb97233afe17008b4bf1aa11a5e808ebbf86709
Chris Strother <61249090+TheChrisBot@users.noreply.github.com>
2023-03-23 12:52:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// 1. Created string for the vault code message.
+
+// 2. Vault code message will appear as an alert!
+
+// 3. Assigned variables
+
+// 4. Added arithmetic operators
+
+// 5. Added an image and some css to make it a little more exciting to look at.
+
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+alert ("You have received this message because you have been chosen to open an important vault.");
+
+//"Comb" will stand for combination.
+var x = 7;
+var y = 3;
+var comb1 = x + y;
+console.log(comb1)
+//Math is fun.
+let a = 4;
+let b = 10;
+let comb2 = a * b;
+console.log(comb2)
+// Decided to use three of the main vairalbe types for learning purposes.
+const c = 69;
+const d = 30;
+const comb3 = c - d;
+console.log(comb3);
+
